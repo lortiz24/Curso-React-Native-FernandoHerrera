2b6a2b3ff5a9806408be5ab7f55a7664d77f20a5
lortiz2022 <107954011+lortiz2022@users.noreply.github.com>
2022-08-17 15:47:20 -0500

    Boton flotante ok restar y ssumar

--- 02-MiPrimeraApp/README.md
+++ 02-MiPrimeraApp/README.md
@@ -67,3 +67,54 @@ Esta es la plantilla para configurarlo
   accessibilityLabel="Learn more about this purple button"
 />
 ```
+## 54. TouchableOpacity
+
+Estaaremos trabajando con touchableOpacity.
+
+touchableOpacity lo hace un poco transparente.
+
+1. Llamamos el componente touchableOpacity y le ponemos como hijo un Text 
+
+2.  Al touchableOpacity le colocamos un onPress que haga setContador +1
+
+3. Ahora crearemos un boton Personalizado con View (Los textos hay que enmarcarlos en un componente de texto, no se muestran asi como asi)
+
+    1. Al view le colocamos un backgroundColor red y un borderRadius de 100
+
+## 55. StyleSheet
+
+¿Como podemos reutilizar el codigo y centralizarlo?
+
+1. Importamos en el ContadorSccreen el StyleSheet
+2. Usualmente se coloca al final del componente, creamos el eso con StyleSheet.create y lo guardamos en una variable llamda styles y le pasamos al metodo como parametro un objeto de la siguiente manera:
+
+```javascript
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:'center',
+    }
+});
+```
+3. En la etiqueta style del componente que queramos estilizar, le pasamos el ``style.container``
+
+
+## 56. Boton personalizado flotante
+
+Este elemento no existe como tal, sino que tenemos que crearlo. Se puede usar un paquete de terceros pero en este ccaso vamos a crearlo nosotros
+
+1. Vamos a comentar el bottonIncrement del style y vamos a crear un style.fab
+    1. Le agregamos un backgroundColor #5856D6
+    2. width de 60
+    3. height de 60
+    4. borderRadius de 100
+    5. justifyContent center y eso hace que se centro arriba y abajo
+2. Agregamos un style.fabText que sirve para el text dentro del touchableOpacity
+    1. color white
+    2. fontSize de 25
+    3. fontWeight de bold
+    4. Le agregamos alignSelf center para que el texto se centro dentro de la bolita 
+3. Creamos dentro del styleSheet un fabLocationBL  que es para el touchableOpacity
+    1. Aqui le colocamos un position absolute, basado en el padre
+    2. bottom en 0, le decimos que lo ponganal final del padre
+    3. right de 0 para que este al inicio contando de derecha a izquierda basado en la posicon del padre
--- 02-MiPrimeraApp/src/screens/ContadorScreens.tsx
+++ 02-MiPrimeraApp/src/screens/ContadorScreens.tsx
@@ -1,30 +1,78 @@
-import React, { useState } from 'react'
-import { Button, Text, View } from 'react-native'
+import React, { useState } from 'react';
+import { Button, Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 export const ContadorScreens = () => {
-    const [contador, setContador] = useState(10)
+    const [contador, setContador] = useState(10);
     return (
         <View
-            style={{
-                flex: 1,
-                justifyContent: 'center'
-            }}
+            style={styles.container}
         >
             <Text
-                style={{
-                    textAlign: 'center',
-                    fontSize: 40,
-                    top: 100,
-                }}
+                style={styles.text}
             >
                 Contador: {contador}
             </Text>
 
-            <Button
-                title="Incrementar"
-                onPress={()=>setContador(1)}
-                
-            />
+
+            <TouchableOpacity
+                style={styles.fabLocationBR}
+                onPress={() => setContador(contador + 1)} >
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>
+                        +1
+                    </Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.fabLocationBL}
+                onPress={() => setContador(contador - 1)} >
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>
+                        -1
+                    </Text>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 40,
+        top: 100,
+    },
+
+    fab: {
+        backgroundColor: '#5856D6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center',
+    },
+    fabText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+    },
+    fabLocationBR: {
+        position: 'absolute',
+        bottom: 20,
+        right: 10,
+    },
+    fabLocationBL: {
+        position: 'absolute',
+        bottom: 20,
+        left: 10,
+    },
+    /* bottonIncrement: {
+        backgroundColor: 'red',
+        borderRadius: 100,
+    } */
+});
